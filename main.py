d6b77526d09b64370e9707ae0710d75bfa175bcf
Reetam Biswas <reetamit>
2025-10-29 12:34:43 -0400

    Initial Commit

---
+++ main.py
@@ -0,0 +1,36 @@
+from fastapi import FastAPI, File, UploadFile
+from PIL import Image
+import torch
+import torch.nn as nn
+from torchvision import models, transforms
+import io
+
+app = FastAPI()
+
+# Load model
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+model = models.efficientnet_b0(weights=None)
+model.features[0][0] = nn.Conv2d(1, 32, kernel_size=3, stride=2, padding=1, bias=False)
+model.classifier[1] = nn.Linear(model.classifier[1].in_features, 4)
+model.load_state_dict(torch.load("./efficientnet_b0.pth", map_location=device))
+model = model.to(device)
+model.eval()
+
+# Define transform
+test_transform = transforms.Compose([
+    transforms.Resize((224, 224)),
+    transforms.ToTensor(),
+    transforms.Normalize(mean=[0.5], std=[0.5])
+])
+
+# Prediction endpoint
+@app.post("/predict")
+async def predict(image: UploadFile = File(...)):
+    image_bytes = await image.read()
+    pil_image = Image.open(io.BytesIO(image_bytes)).convert('L')
+    input_tensor = test_transform(pil_image).unsqueeze(0).to(device)
+
+    with torch.no_grad():
+        output = model(input_tensor)
+        _, predicted = torch.max(output, 1)
+        return {"label": int(predicted.item())}
